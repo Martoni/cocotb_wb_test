1eee030820aed7f0ed9f770057fb3518e92af858
Fabien Marteau <fabien.marteau@armadeus.com>
2016-05-05 21:53:54 +0200

    all verilog sources

--- cocotb/test_PWM.py
+++ cocotb/test_PWM.py
@@ -1,2 +1,17 @@
 #!/bin/python
 import cocotb
+
+from cocotb.triggers import Timer
+
+@cocotb.test()
+def my_first_test(dut):
+    """
+    Try accessing the design
+    """
+    dut.log.info("Running test!")
+    for cycle in range(10):
+        dut.clk = 0
+        yield Timer(1000)
+        dut.clk = 1
+        yield Timer(1000)
+    dut.log.info("Running test!")
